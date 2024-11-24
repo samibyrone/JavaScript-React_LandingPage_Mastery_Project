0db265661b1e43359125efd688ed9cfe7500a0e8
samibyrone <samsonibironke2@gmail.com>
2024-11-24 08:53:40 +0100

    landing-page commit

---
+++ src/containers/index.js
@@ -0,0 +1,6 @@
+export {default as Blog} from './blog/Blog';
+export {default as Feature} from './features/Features';
+export {defualt as Footer} from './footer/Footer';
+export {defualt as header} from './header/Header';
+export {default as Possibility} from './possibility/Possibility';
+export {defualt as WhatGPT3} from './whatGPT3/WhatGPT3';
